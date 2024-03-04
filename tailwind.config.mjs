8c0d1a4aaf87582c955a524168b61b38d75012d4
CarlosRivera4726 <carlosrivera_-717hotmail.com>
2024-03-04 13:29:55 -0500

    solved toogleTheme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,14 @@ export default {
 	darkMode: 'class',
 	theme: {
 		extend: {},
+		screens: {
+			'xsm': '375px',
+			'sm': '640px',
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1280px',
+			'2xl': '1536px',
+		}
 	},
 	plugins: [],
 }
